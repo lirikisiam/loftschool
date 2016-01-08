7656d4e2f1117a29ca6fe02935cc182b15e69321
Kirill Tryshchenkov <tk.kirill@gmail.com>
2016-01-08 17:02:27 +0200

    Form validate fix

---
+++ app/js/main.js
@@ -0,0 +1,25 @@
+/*
+		Modals
+ */
+;(function($) {
+        $(function() {
+            $('#modal').bind('click', function(e) {
+                e.preventDefault();
+                $('.modal').bPopup({
+                });
+            });
+         });
+     })(jQuery);
+
+ /*
+		Validate jQ
+ */
+
+ $(".modal__form").validate({
+	rules: {
+		name: {
+			required: true
+		}
+	}
+ });
+
